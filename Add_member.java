d75bd0f8e7be8fadbaff2b08366f59e6f518f128
darel-developer <122864725+darel-developer@users.noreply.github.com>
2024-02-21 15:31:36 +0100

    modification Add_member

--- Add_member.java
+++ Add_member.java
@@ -22,37 +22,17 @@ public class Add_member {
 
         JButton membreButton = createStyledButton("New Member", "./images/new member.png");
         JButton packButton = createStyledButton("Add Pack", "/path/to/pack.png");
-        JButton addCoachButton = createStyledButton("Add Coach", "/path/to/pack.png");
 
-        JButton listePackButton = createStyledButton("Pack List", "./images/list of members.png");
-        JButton listeCoachButton = createStyledButton("Coach List", "./images/list of members.png");
-        JButton listeMemberButton = createStyledButton("Member List", "./images/list of members.png");
-        JButton deconnexionButton = createStyledButton("Déconnexion", "./images/logout.png");
 
         leftPanel.add(membreButton);
         leftPanel.add(packButton);
-        leftPanel.add(addCoachButton);
-        leftPanel.add(listePackButton);
-        leftPanel.add(listeCoachButton);
-        leftPanel.add(listeMemberButton);
-        leftPanel.add(deconnexionButton);
+
 
         JPanel rightPanel = new JPanel();
         rightPanel.setLayout(new GridLayout(6, 2));
         rightPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
 
-        // Ajouter deux boutons à la partie droite
-        JButton rightButton1 = createStyledButton("Right Button 1", "");
-        JButton rightButton2 = createStyledButton("Right Button 2", "");
-
-        // Ajouter de l'espace entre les boutons
-        int spaceWidth = 10; // Ajustez la largeur de l'espace selon vos besoins
-        JPanel spacePanel = new JPanel();
-        spacePanel.setPreferredSize(new Dimension(spaceWidth, 1)); // Largeur de l'espace
-        rightPanel.add(rightButton1);
-        rightPanel.add(spacePanel);
-        rightPanel.add(rightButton2);
 
 
         mainPanel.add(leftPanel, BorderLayout.WEST);
